07a1c32544ac29acc111e0d684a9e17d2cbbc797
codo-ajmalnk <ai.codomail@gmail.com>
2025-06-02 19:39:07 +0530

    updated

--- api/get_project_testers.php
+++ api/get_project_testers.php
@@ -1,7 +1,7 @@
 <?php
+require_once __DIR__ . '/../config/cors.php';
 header('Content-Type: application/json');
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+
 
 require_once __DIR__ . '/../config/database.php';
 $database = new Database();
--- api/send-fcm-message.php
+++ api/send-fcm-message.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/../config/cors.php';
 require_once __DIR__ . '/users/UserController.php';
 
 $controller = new UserController();
