4d5a7528be638868a5a10ebe88ad1116f2a721d2
codo-ajmalnk <ai.codomail@gmail.com>
2025-06-03 18:53:14 +0530

    updated

--- api/notifications/get_recent.php
+++ api/notifications/get_recent.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('log_errors', 1);
-ini_set('error_log', __DIR__ . '/../../logs/notifications_error.log');
+// ini_set('error_log', __DIR__ . '/../../logs/notifications_error.log');
 
 require_once __DIR__ . '/../../config/cors.php';
 require_once __DIR__ . '/../BaseAPI.php';
