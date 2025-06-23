c165f0f8a69892c5c52df30de0d8e1b194c3a891
codo-ajmalnk <ai.codomail@gmail.com>
2025-06-23 21:21:33 +0530

    new announsment

--- api/bugs/test-route.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json');
-echo json_encode([
-    "success" => true,
-    "message" => "Bug route test successful",
-    "endpoint" => "test-route",
-    "timestamp" => date('Y-m-d H:i:s')
-]);
-?> 
