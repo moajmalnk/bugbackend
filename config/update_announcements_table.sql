4d654263b9cfc63dac95ac5a0b6ef1875331823e
codo-ajmalnk <ai.codomail@gmail.com>
2025-06-23 21:21:33 +0530

    new announsment

--- config/update_announcements_table.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE announcements
-ADD COLUMN last_broadcast_at DATETIME DEFAULT NULL; 
--- database/find_foreign_keys.sql
+++
@@ -1,4 +0,0 @@
--- Find actual foreign key constraint names
--- Run this first to see what foreign keys exist
-
-SHOW CREATE TABLE project_activities; 
