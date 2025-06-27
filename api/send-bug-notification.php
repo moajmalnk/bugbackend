0a39e339419cbdaa6d3820f4887e1865f51b7e0d
codo-ajmalnk <ai.codomail@gmail.com>
2025-06-27 18:13:59 +0530

    updated

--- api/send-bug-notification.php
+++ api/send-bug-notification.php
@@ -68,7 +68,3 @@ try {
     http_response_code(500);
     echo json_encode(['success' => false, 'message' => 'Server error: ' . $e->getMessage()]);
 }
-
-file_put_contents('debug_email.txt', print_r($data, true), FILE_APPEND);
-
-error_log("send-bug-notification called: " . json_encode($data));
