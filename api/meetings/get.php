59fc630f68596e1080802767f187b097caaa0e05
moajmalnk <moajmalnk@gmail.com>
2025-09-14 12:06:56 +0530

    updated by nk

---
+++ api/meetings/get.php
@@ -0,0 +1,14 @@
+<?php
+header('Content-Type: application/json');
+require_once __DIR__ . '/MeetingController.php';
+
+$code = $_GET['code'] ?? '';
+if ($code === '') {
+    echo json_encode(['success' => false, 'error' => 'code required']);
+    exit;
+}
+
+$controller = new MeetingController();
+$response = $controller->getMeetingByCode($code);
+echo json_encode($response);
+exit;
