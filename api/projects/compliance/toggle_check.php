f0b984f61c463621a0c1f78d5c5587561b995423
moajmalnk <moajmalnk@gmail.com>
2026-07-07 17:10:39 +0530

    u pdated by nk

--- api/projects/compliance/toggle_check.php
+++ api/projects/compliance/toggle_check.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../../config/cors.php';
+require_once __DIR__ . '/../../../config/cors.php';
 require_once __DIR__ . '/../ProjectComplianceController.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
